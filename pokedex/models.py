1d84172382da25ce1149ad583554469b9df310b2
jayelsea <fuirner@gmail.com>
2024-07-06 10:12:19 -0500

    Final

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Clases que ayudan a gestionar la base de datos
+
+class Pokemon (models.Model):
+    name = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, null=False)
+    weight = models.IntegerField(null=False)
+    height = models.IntegerField(null=False)
+    
+    def __str__(self) -> str:
+        return self.name
