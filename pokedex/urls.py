7b1721d563916032b6c5adb2043ac07632e9b744
jayelsea <fuirner@gmail.com>
2024-07-10 17:48:36 -0500

    code

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     #Mantener id pokemon
-    path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"),
+    path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon_id"),
 ]
